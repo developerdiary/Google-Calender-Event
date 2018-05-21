b977780813d278298a46f800f57d8d1529ae5cb0
Developer's Diary <30465804+developerdiary@users.noreply.github.com>
2018-05-21 15:08:29 +0530

    Add files via upload
    First commit php google calender insert event

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', 'xxxxxxxxxxxxxxx');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', 'xxxxxxxxxxxxxxx');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'xxxxxxxxxxxxxxx');
+
+?>
